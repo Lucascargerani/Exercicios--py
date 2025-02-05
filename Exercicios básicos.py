ef7426a2b3568174c86a9c7dacae7ca3fe9f5561
Lucascargerani <lucascargerani@gmail.com>
2025-02-05 12:49:16 -0300

    update_1

---
+++ Exercicios básicos.py
@@ -0,0 +1,31 @@
+# nome = "Lucas"
+# idade = 25
+# cidade = "São Paulo"
+#
+# apresentação = f"Olá meu nome é {nome}. Tenho {idade} anos e moro em {cidade}."
+# print(apresentação)
+#
+# --------------------------------------------------------------------------------------------------
+
+# numeros = [10, 20, 30, 40, 50]
+#
+# soma = 0
+# for numero in numeros:
+#     soma += numero
+#
+# media = soma / len(numeros)
+# print("A média será de: ", media)
+
+
+#-----------------------------------------------------------------------------------------------------
+
+
+
+
+gastos_joao = [300, 500, 200, 800]
+gastos_pedro = [200, 400, 500, 700]
+
+total = gastos_joao >= gastos_pedro
+
+if ()
+
